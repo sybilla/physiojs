1050317040f5a40fb6279f1e5dd3c769b3af6efd
Sławomir Hus <slawomir.hus@sybillatechnologies.com>
2018-09-26 15:06:21 +0200

    add GroupId to label

--- src/data/Label.ts
+++ src/data/Label.ts
@@ -2,10 +2,11 @@ export class Label {
     constructor(
         public position: number[], 
         public type: "Flag" | "Circle" | "Line",
-        public description: string) {
+        public description: string,
+        public GroupId: number) {
     }
 
     public static fromDTO(obj: Object) {
-        return new Label(obj["Position"], obj["Type"], obj["Description"]);
+        return new Label(obj["Position"], obj["Type"], obj["Description"], obj["GroupId"]);
     }
 }
